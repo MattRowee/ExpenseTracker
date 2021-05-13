d35fbcc7985bcf8b8f9c18789ccb912ae7d1c93d
Matthew Rowe <matthew.rowe@ibm.com>
2021-05-13 13:09:43 -0400

    completed expense tracker project

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import ExpenseFilter from "../ExpenseFilter/ExpenseFilter";
+import ExpenseFilter from "./ExpenseFilter/ExpenseFilter";
 import Card from "../UI/Card";
 import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -22,6 +23,7 @@ const Expenses = (props) => {
     <li>
     <Card className="expenses">
       <ExpenseFilter selected={filteredYear} onChangeFilter={filterByYear} />
+      <ExpensesChart expenses={filteredExpenses}/>
       <ExpensesList items={filteredExpenses} />
     </Card>
     </li>
